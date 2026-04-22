eaaf35723f537eabf69345878dc817874b44239a
edricmedrano <edricmedrano12@gmail.com>
2026-04-22 19:10:16 -0400

    Student.java

---
+++ Student.java
@@ -0,0 +1,32 @@
+class Person {
+    String name;
+    int age;
+
+    Person(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    public void introduce() {
+        System.out.println("Hi, my name is " + name + " and I am " + age + " years old.");
+    }
+}
+
+public class Student extends Person {
+    String school;
+
+    Student(String n, int a, String s) {
+        super(n, a);
+        school = s;
+    }
+
+    public void introduce() {
+        System.out.println("Hi, my name is " + name + " and I am " + age + " years old.");
+        System.out.println("I attend " + school + ".");
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student("Edric", 18, "Middlesex College");
+        s1.introduce();
+    }
+}
